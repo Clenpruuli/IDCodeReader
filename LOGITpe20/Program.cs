ae4b3967478b9402d894bf114057e2cd90cd2f06
Opilane TTHK <opilane@tthk.ee>
2020-10-15 11:33:34 +0300

    first commit

---
+++ LOGITpe20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LOGITpe20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayLOGIT("LOGITPE20");
+        }
+
+        public static void DisplayLOGIT(string LOGITpe20)
+        {
+            string logit = LOGITpe20.Substring(0, 5);
+            string pe = LOGITpe20.Substring(5, 2);
+            string twenty = LOGITpe20.Substring(7, 2);
+            Console.WriteLine($"-->{logit}");
+            Console.WriteLine($"-->{pe}");
+            Console.WriteLine($"-->{twenty}");
+        }
+    }
+}
---
+++ SubString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SubString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sumString = "interpolation";
+            Console.WriteLine($"{sumString.Length}");
+            string newString = sumString.Substring(0, 5);
+            Console.WriteLine(newString);
+            string polationString = sumString.Substring(5, 8);
+            Console.WriteLine(polationString); 
+        }
+    }
+}
