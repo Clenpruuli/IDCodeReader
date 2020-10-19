0f64edbf89410469f0c466da0b0711a632bcc984
Opilane TTHK <opilane@tthk.ee>
2020-10-19 15:02:47 +0300

    first commit

--- IDCodeReader/Program.cs
+++ IDCodeReader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace IDCodeReader
 {
@@ -7,10 +8,12 @@ namespace IDCodeReader
         static void Main(string[] args)
         {
             Console.WriteLine("Enter your ID code:");
-            string userID = Console.ReadLine();
-            if(Validate(userID))
+            string usersID = Console.ReadLine();
+            if(Validate(usersID))
             {
                 Console.WriteLine("Welcome!");
+                HelloUser(usersID);
+
             }else
             {
                 Console.WriteLine("Wrong format.Try again.");
@@ -40,11 +43,23 @@ namespace IDCodeReader
                 return false;
             }
 
+            
 
 
         }
 
+        public static void HelloUser(string idCode)
+        {
+            int FirstNum = Int32.Parse(idCode[0].ToString());
+            if(FirstNum == 1 || FirstNum == 3 || FirstNum == 5)
+            {
+                Console.WriteLine("Hello,sir!");
 
+            }else if (FirstNum == 2 || FirstNum == 4 || FirstNum == 6)
+            {
+                Console.WriteLine("Hello,Madam!");
+            }
+        }
 
     }
 }
---
+++ YearValidator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace YearValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your year of birth");
+            string userInput = Console.ReadLine();
+
+            if(Validate(userInput))
+            {
+
+            }
+        }
+
+        public static bool Validate(string yearInput)
+        {
+            if(yearInput.Length == 4)
+                try
+                {
+                    Int32.Parse(yearInput);
+                    Console.WriteLine("All good. ");
+                    return true;
+                } catch(Exception error)
+                {
+                    Console.WriteLine(error);
+                    return false;
+                } else
+                {
+                    Console.WriteLine("Wrong format. ");
+                    return false;
+                }
+        } 
+        
+        public static void CalculateAge(string yearInput)
+        {
+            int year = Int32.Parse(yearInput);
+
+            DateTime now = DateTime.Now;
+            string currentYear = now.Year.ToString();
+            int yearnow = Int32.Parse(currentYear);
+
+            Console.WriteLine($"You are {yearnow} - {year} years old");
+        }
+        
+    }
+}
